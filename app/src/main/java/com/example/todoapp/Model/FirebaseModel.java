be3ca0d6b493031ecad2ac3bcc31ad352c7f5b44
Raees <muhammadraeesahmed38@gmail.com>
2022-08-31 22:59:11 +0500

    access modifers added

--- app/src/main/java/com/example/todoapp/Model/FirebaseModel.java
+++ app/src/main/java/com/example/todoapp/Model/FirebaseModel.java
@@ -29,6 +29,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class FirebaseModel {
+    // private variables
     private static FirebaseModel single_instance = null;
     private final FirebaseFirestore db;
     private final StorageReference storageRef;
